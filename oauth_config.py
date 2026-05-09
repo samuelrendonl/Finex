b30e3b4b67c466e39617f6d964bab2a96214cb5a
Samuel Rendón <samuellozanorendon@gmail.com>
2026-05-08 21:02:58 -0500

    Fix secrets

---
+++ oauth_config.py
@@ -0,0 +1,21 @@
+from authlib.integrations.flask_client import OAuth
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+oauth = OAuth()
+
+def init_oauth(app):
+
+    oauth.init_app(app)
+
+    oauth.register(
+        name='google',
+        client_id=os.getenv("GOOGLE_CLIENT_ID"),
+        client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+        server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+        client_kwargs={
+            'scope': 'openid email profile'
+        }
+    )
